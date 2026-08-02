28021c02d5f002c3c93a08fc2b6017cf8bdde789
Harsh <harshgpednekar@gmail.com>
2026-08-02 12:52:23 +0530

    Fixed Supabase URL

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cnrjetqzqzqqliopelqz.supabase.co' 
+// This is your actual, real Supabase project URL!
+const supabaseUrl = 'https://agjktlotjbvlsxpgrjdf.supabase.co' 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnamt0bG90amJ2bHN4cGdyamRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NjYzNTMsImV4cCI6MjEwMTE0MjM1M30.nVDFittt609MFD_doCCuOgMordTBvxaUUGJBdZt_p9A'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
